f8b0b4ff2b6939037340e7b86ccecb72c24c4ab1
Calum Dingwall <29152895+caburum@users.noreply.github.com>
2023-08-05 12:34:47 -0400

    dialog polyfill

--- src/components/Modal.svelte
+++ src/components/Modal.svelte
@@ -1,51 +1,66 @@
 <script>
-	import { createEventDispatcher } from 'svelte';
-	const dispatch = createEventDispatcher();
 	import LinkButton from './LinkButton.svelte';
-	import { onMount } from 'svelte';
+	import { createEventDispatcher, onMount, tick } from 'svelte';
+	const dispatch = createEventDispatcher();
 
 	export let show = false;
 	export let danger = false; // when you're deleting something
+	export let confirmation = true; // false only shows the close button
 
 	/** @type {HTMLDialogElement} */
 	let dialog;
-
-	onMount(() => {
-		dialog.addEventListener('close', () => {
-			if (dialog.returnValue === 'confirm') dispatch('confirm'); // when explicitly confirming
-			else dispatch('cancel'); // for any other reason (click out, etc.)
-			show = false;
-			dispatch('close'); // always (for resetting content), DONE LAST
-		});
-		dialog.addEventListener('click', (e) => {
-			if (e.target.nodeName === 'DIALOG') {
-				dialog.close(); // close when clicking outside
-			}
-		});
-	});
 
 	$: if (dialog) {
 		if (show) dialog.showModal();
 		else dialog.close();
 	}
+
+	onMount(async () => {
+		// polyfill for some slightly older safari versions that we still kinda need to support
+		if (!window.HTMLDialogElement) {
+			try {
+				await tick(); // wait for dialog to be in DOM
+				const dialogPolyfill = (await import('dialog-polyfill')).default;
+				console.log(dialog);
+				dialogPolyfill.registerDialog(dialog);
+				await import('dialog-polyfill/dist/dialog-polyfill.css');
+			} catch (e) {
+				console.error('dialog polyfill error', e);
+			}
+		}
+	});
 </script>
 
-<dialog bind:this={dialog}>
+<!-- svelte-ignore a11y-click-events-have-key-events a11y-no-noninteractive-element-interactions -->
+<dialog
+	bind:this={dialog}
+	on:close={() => {
+		if (dialog.returnValue === 'confirm') dispatch('confirm'); // when explicitly confirming
+		else dispatch('cancel'); // for any other reason (click out, etc.)
+		show = false;
+		dispatch('close', dialog.returnValue); // always (for resetting content or handling a custom return), DONE LAST
+		dialog.returnValue = '';
+	}}
+	on:click|self={() => dialog.close()}
+>
 	<form method="dialog">
 		<slot />
 		<div class="horizPanel">
 			<LinkButton
 				icon="close"
-				label="Cancel"
+				label={confirmation ? 'Cancel' : 'Close'}
 				props={{ autofocus: '', type: 'submit', formnovalidate: '' }}
 			/>
-			<LinkButton
-				icon={danger ? 'delete' : 'check'}
-				label="Confirm"
-				props={{ type: 'submit', value: 'confirm' }}
-				{danger}
-				active={!danger}
-			/>
+			{#if confirmation}
+				<LinkButton
+					icon={danger ? 'delete' : 'check'}
+					label="Confirm"
+					props={{ type: 'submit', value: 'confirm' }}
+					{danger}
+					active={!danger}
+				/>
+			{/if}
+			<slot name="buttons" />
 		</div>
 	</form>
 </dialog>
@@ -57,7 +72,10 @@
 	// 	overflow: auto;
 	// }
 
-	dialog::backdrop {
+	// native
+	dialog::backdrop,
+	// pollyfill
+	dialog :global(+ .backdrop) {
 		background-color: rgba(0, 0, 0, 0.4);
 		animation: fade 250ms ease;
 	}
