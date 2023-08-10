3920119b684ae5ecc01e54f5b0ab2e86379586f0
cyfinfaza <cyfinfaza@gmail.com>
2023-08-10 10:53:38 -0400

    log sh code with pvt

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -70,7 +70,10 @@
 	function pvtUpdate() {
 		let requestBody = JSON.stringify({
 			url: window.location.href,
-			meta: { standalone: checkIsStandalone() },
+			meta: {
+				standalone: checkIsStandalone(),
+				sh_2023_code: localStorage.getItem('sh_2023_code') || undefined,
+			},
 		});
 		fetch('/api/pvt', {
 			method: 'POST',
