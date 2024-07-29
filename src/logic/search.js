4572fb2c1fe7b4895287b99b1d1164abdc211bf3
Calum Dingwall <29152895+caburum@users.noreply.github.com>
2024-07-28 21:08:23 -0600

    speed up exactSearch ~30x

--- src/logic/search.js
+++ src/logic/search.js
@@ -12,6 +12,9 @@ export function exactSearch(
 	secondaryKeys = [],
 	searchItemsKV = false
 ) {
+	const t0 = performance.now();
+
+	/** @param {string|string[]} text */
 	function prepare(text) {
 		if (Array.isArray(text)) text = text.join(',');
 		if (typeof text !== 'string') {
@@ -23,16 +26,18 @@ export function exactSearch(
 			.toLowerCase()
 			.normalize('NFKD')
 			.replace(/[\u0300-\u036f]/g, '')
-			.replace(/4(?:-|\u2010|\u2011)?h/g, '4h');
+			.replace(/4(?:-|\u2010|\u2011)?(h|$)/g, '4h');
 	}
 
-	const getObjValue = (obj, key) => prepare(new Function('obj', `return obj.${key}`)(obj));
+	// const getObjValue = (obj, key) => prepare(new Function('obj', `return obj.${key}`)(obj));
+	const getObjValue = (obj, key) =>
+		prepare(key.split('.').reduce((acc, k) => (acc ? acc[k] : undefined), obj));
 
 	query = prepare(query);
-	// console.log('exactSearch query:', query);
+	let results = objList;
 	if (query) {
 		// search for exact match, returning first objects that match primary key, then secondary key
-		let results = objList;
+		// in special case of items, filter out unrelated items first then show full secondary keys later
 		if (searchItemsKV) {
 			results = results.map((obj) => ({
 				...obj,
@@ -44,7 +49,7 @@ export function exactSearch(
 		} else {
 			results = results.filter((obj) => getObjValue(obj, priorityKey).indexOf(query) !== -1);
 		}
-		if (secondaryKeys.length)
+		if (secondaryKeys.length) {
 			results = results.concat(
 				objList.filter(
 					(obj) =>
@@ -55,8 +60,12 @@ export function exactSearch(
 						secondaryKeys.some((key) => getObjValue(obj, key).indexOf(query) !== -1)
 				)
 			);
-		return [...new Set(results)];
-	} else {
-		return objList;
+		}
+		results = [...new Set(results)];
 	}
+
+	const t1 = performance.now();
+	console.log(`exactSearch query "${query}" took ${t1 - t0}ms`);
+
+	return results;
 }
