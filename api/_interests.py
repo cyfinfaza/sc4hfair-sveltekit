17f317d919289d778b7d365b628a5f6db3a85523
Calum Dingwall <caburum@users.noreply.github.com>
2025-06-17 22:52:15 -0400

    tweak apis

--- api/_interests.py
+++ api/_interests.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask import Blueprint, g
 
 from ._util import auth_required, mongo, success_json, error_json
@@ -7,17 +8,16 @@ bp = Blueprint('interests', __name__, url_prefix='/interests')
 db = mongo.interests
 interests = db.interests
 
-# todo: add year to schema and filtering
-
 @bp.before_request
 @auth_required
 def before_request():
+	g.current_year = datetime.now().year
 	pass
 
 @bp.route('/', methods=['GET'])
 def get_interests():
 	res = interests.aggregate([
-		{'$match': {'user_id': g.user_id}},
+		{'$match': {'user_id': g.user_id, 'year': g.current_year}},
 		{'$group': {'_id': None, 'interests': {'$addToSet': '$slug'}}},
 		{'$project': {'_id': False, 'interests': True}}
 	])
@@ -30,28 +30,24 @@ def get_interests():
 
 @bp.route('/', methods=['DELETE'])
 def delete_all_interests():
-	result = interests.delete_many({'user_id': g.user_id})
+	result = interests.delete_many({'user_id': g.user_id, 'year': g.current_year})
 
 	return success_json(result.deleted_count)
 
 @bp.route('/<slug>', methods=['PUT'])
 def add_interest(slug):
-	result = interests.update_one(
-		{'user_id': g.user_id, 'slug': slug},
-		{
-			'$setOnInsert': {
-				'user_id': g.user_id,
-				'slug': slug,
-			}
-		},
-		upsert=True
-	)
+	doc = {
+		'user_id': g.user_id,
+		'slug': slug,
+		'year': g.current_year
+	}
+	result = interests.update_one(doc, {'$setOnInsert': doc}, upsert=True)
 
 	return success_json(status=201 if result.upserted_id is not None else 200)
 
 @bp.route('/<slug>', methods=['DELETE'])
 def delete_interest(slug):
-	result = interests.delete_one({'user_id': g.user_id, 'slug': slug})
+	result = interests.delete_one({'user_id': g.user_id, 'slug': slug, 'year': g.current_year})
 
 	if result.deleted_count == 0:
 		return error_json(status=404)
--- api/_webpush.py
+++ api/_webpush.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from dateutil.tz import UTC
-from flask import Blueprint, request, session
+from flask import current_app, Blueprint, g, request, session
+from functools import wraps
 import json
 from os import environ
 from pymongo import ReturnDocument
@@ -66,161 +67,169 @@ def test_webpush(sub_info, id):
 
 subscriptionIdProjection = {'_id': False, 'subscriptionId': {'$toString': '$_id'}}
 
+def prepare_sub(sub_required=True):
+	def decorator(f):
+		@wraps(f)
+		def wrapper(*args, **kwargs):
+			data = request.get_json() or {}
+
+			g.sub_info = data.get('subscription')
+			if sub_required and g.sub_info is None:
+				return error_json('No subscription info')
+			
+			g.oldSubscription = data.get('oldSubscription')
+
+			print('sub_info', g.sub_info)
+			print('pvt id', 'track_id' in session and session['track_id'])
+
+			existing = subscriptionsCollection.find_one(
+				{'subscription_info': g.sub_info, 'valid': {'$ne': False}, 'registered': {'$ne': False}},
+				projection=subscriptionIdProjection
+			)
+			g.alreadyExists = existing is not None
+			g.subscriptionId = existing['subscriptionId'] if existing else None
+
+			return f(*args, **kwargs)
+		wrapper.__name__ = f.__name__
+		return wrapper
+	return decorator
+
 # add (or check if dry) subscription
-@bp.route('/subscribe', methods=['POST'], endpoint='subscribe')
-# remove subscription
-@bp.route('/unsubscribe', methods=['POST'], endpoint='unsubscribe')
-# pushsubscriptionchange event
-@bp.route('/resubscribe', methods=['POST'], endpoint='resubscribe')
-def main():
-	'''main route with common logic'''
+@bp.route('/subscribe', methods=['POST'])
+@prepare_sub()
+def subscribe():
+	test_id = str(uuid4())
 
-	data = request.get_json() or {}
+	dry = 'dry' in request.args
 
-	sub_info = data.get('subscription')
-	if request.endpoint != 'resubscribe':
-		if sub_info is None: return error_json('No subscription info')
-	
-	oldSubscription = data.get('oldSubscription')
+	# check sub by sending test notif if necessary
+	# if we're dry, we can just reply if the sub exists
+	if (not dry) and (not test_webpush(g.sub_info, test_id)):
+		return error_json('Test failed')
 
-	print('sub_info', sub_info)
-	print('pvt id', 'track_id' in session and session['track_id'])
+	result = 'noChange'
 
-	existing = subscriptionsCollection.find_one(
-		{'subscription_info': sub_info, 'valid': {'$ne': False}, 'subscribed': {'$ne': False}},
-		projection=subscriptionIdProjection
-	)
-	alreadyExists = existing is not None
-	subscriptionId = existing['subscriptionId'] if existing else None
-
-	if request.endpoint == 'subscribe':
-		test_id = str(uuid4())
-
-		dry = 'dry' in request.args
-
-		# check sub by sending test notif if necessary
-		# if we're dry, we can just reply if the sub exists
-		if (not dry) and (not test_webpush(sub_info, test_id)):
-			return error_json(message='Test failed')
-
-		result = 'noChange'
-
-		# modify db if necessary
-		if not dry:
-			update = {'$set': {
-				'created': datetime.now(tz=UTC),
-				'subscription_info': sub_info,
-				'user_agent': str(request.user_agent),
-				'track_id': 'track_id' in session and session['track_id'] or None,
-				'valid': None, # we just got it, it should be fine
-				'registered': True # just subscribed
-			}}
-
-			oldExisting = subscriptionsCollection.find_one(
-				{'subscription_info': {'$exists': True, '$ne': None, '$eq': oldSubscription}},
-				projection={'_id': True, 'subscriptionId': {'$toString': '$_id'}}
-			) if oldSubscription is not None else None
-
-			# overwrite old subscription with new (will update if old is same as new)
-			if oldExisting:
-				sub = subscriptionsCollection.find_one_and_update(
-					{'_id': oldExisting['_id']},
-					update,
-					projection=subscriptionIdProjection,
-					upsert=False,
-					return_document=ReturnDocument.AFTER
-				)
-				subscriptionId = sub['subscriptionId']
-				result = 'oldExistingUpdated'
-
-			# create new subscription
-			else:
-				sub = subscriptionsCollection.find_one_and_update(
-					{'subscription_info': sub_info},
-					update,
-					projection=subscriptionIdProjection,
-					upsert=True,
-					return_document=ReturnDocument.AFTER
-				)
-				subscriptionId = sub['subscriptionId']
-				result = 'oldUpdated' if alreadyExists else 'newCreated'
-
-		return success_json(data={
-			'alreadyExists': alreadyExists,
-			'registered': alreadyExists if dry else True, # if the subscription is in the db
-			'testId': None if dry else test_id,
-			'subscriptionId': subscriptionId,
-			'result': result
-		})
-
-	elif request.endpoint == 'unsubscribe':
-		if not alreadyExists:
-			return error_json(data={
-				'alreadyExists': False,
-			}, message='Subscription does not exist')
-
-		sub = subscriptionsCollection.update_one(
-			{'subscription_info': sub_info},
-			{'$set': {
-				'registered': False,
-			}},
-			upsert=False
-		)
-
-		return success_json(data={
-			'registered': False,
-			'subscriptionId': subscriptionId
-		})
-
-	elif request.endpoint == 'resubscribe':
-		result = 'noChange'
-
-		if oldSubscription is None:
-			return error_json('No old subscription info')
-
-		subscriptionId = None
-
-		query = {'subscription_info': {
-			'$exists': True,
-			'$ne': None,
-			'$eq': oldSubscription
+	# modify db if necessary
+	if not dry:
+		update = {'$set': {
+			'created': datetime.now(tz=UTC),
+			'subscription_info': g.sub_info,
+			'user_agent': str(request.user_agent),
+			'track_id': 'track_id' in session and session['track_id'] or None,
+			'valid': None, # we just got it, it should be fine
+			'registered': True # just subscribed
 		}}
-		if oldSubscription and 'endpoint' in oldSubscription:
-			query = {'subscription_info': {
-				'endpoint': oldSubscription['endpoint']
-			}}
 
-		# try updating the old entry if we can, otherwise create a new entry
-		if sub_info and not alreadyExists: # newSubscription
+		oldExisting = subscriptionsCollection.find_one(
+			{'subscription_info': {'$exists': True, '$ne': None, '$eq': g.oldSubscription}},
+			projection={'_id': True, 'subscriptionId': {'$toString': '$_id'}}
+		) if g.oldSubscription is not None else None
+
+		# overwrite old subscription with new (will update if old is same as new)
+		if oldExisting:
 			sub = subscriptionsCollection.find_one_and_update(
-				query,
-				{'$set': {
-					'created': datetime.now(tz=UTC),
-					'subscription_info': sub_info,
-					'user_agent': str(request.user_agent),
-					'track_id': 'track_id' in session and session['track_id'] or None,
-					'valid': None
-				}},
-				projection={'_id': False, 'subscriptionId': {'$toString': '$_id'}},
+				{'_id': oldExisting['_id']},
+				update,
+				projection=subscriptionIdProjection,
+				upsert=False,
+				return_document=ReturnDocument.AFTER
+			)
+			g.subscriptionId = sub['subscriptionId']
+			result = 'oldExistingUpdated'
+
+		# create new subscription
+		else:
+			sub = subscriptionsCollection.find_one_and_update(
+				{'subscription_info': g.sub_info},
+				update,
+				projection=subscriptionIdProjection,
 				upsert=True,
 				return_document=ReturnDocument.AFTER
 			)
-			if sub.upserted_id is None:
-				result = 'oldUpdated'
-			else:
-				result = 'newCreated'
-			subscriptionId = sub['subscriptionId']
+			g.subscriptionId = sub['subscriptionId']
+			result = 'oldUpdated' if g.alreadyExists else 'newCreated'
 
-		# if we only have an old subscription
-		elif oldSubscription and 'endpoint' in oldSubscription:
-			op = subscriptionsCollection.update_one(query, {'$set': {
-				'valid': False,
-			}})
-			if op.modified_count > 0:
-				result = 'oldUpdated'
+	return success_json(data={
+		'alreadyExists': g.alreadyExists,
+		'registered': g.alreadyExists if dry else True, # if the subscription is in the db
+		'testId': None if dry else test_id,
+		'subscriptionId': g.subscriptionId,
+		'result': result
+	})
 
-		return success_json(data={
-			'result': result,
-			'alreadyExists': alreadyExists,
-			'subscriptionId': sub['subscriptionId'] if not alreadyExists else None
-		})
+# remove subscription
+@bp.route('/unsubscribe', methods=['POST'])
+@prepare_sub()
+def unsubscribe():
+	if not g.alreadyExists:
+		return error_json(data={
+			'alreadyExists': False,
+		}, message='Subscription does not exist')
+
+	sub = subscriptionsCollection.update_one(
+		{'subscription_info': g.sub_info},
+		{'$set': {
+			'registered': False,
+		}},
+		upsert=False
+	)
+
+	current_app.logger.info(f'Unsubscribed {g.subscriptionId} {sub.matched_count}', sub)
+
+	return success_json(data={
+		'registered': False,
+		'subscriptionId': g.subscriptionId
+	})
+
+# pushsubscriptionchange event
+@bp.route('/resubscribe', methods=['POST'])
+@prepare_sub(sub_required=False)
+def resubscribe():
+	result = 'noChange'
+
+	if g.oldSubscription is None:
+		return error_json('No old subscription info')
+
+	g.subscriptionId = None
+
+	query = {'subscription_info': {
+		'$exists': True,
+		'$ne': None,
+		'$eq': g.oldSubscription
+	}}
+	if g.oldSubscription and 'endpoint' in g.oldSubscription:
+		query = {'subscription_info': {
+			'endpoint': g.oldSubscription['endpoint']
+		}}
+
+	# try updating the old entry if we can, otherwise create a new entry
+	if g.sub_info and not g.alreadyExists: # newSubscription
+		sub = subscriptionsCollection.find_one_and_update(
+			query,
+			{'$set': {
+				'created': datetime.now(tz=UTC),
+				'subscription_info': g.sub_info,
+				'user_agent': str(request.user_agent),
+				'track_id': 'track_id' in session and session['track_id'] or None,
+				'valid': None
+			}},
+			projection={'_id': False, 'subscriptionId': {'$toString': '$_id'}},
+			upsert=True,
+			return_document=ReturnDocument.AFTER
+		)
+		result = 'oldUpdated' if sub.upserted_id is None else 'newCreated'
+		g.subscriptionId = sub['subscriptionId']
+
+	# if we only have an old subscription
+	elif g.oldSubscription and 'endpoint' in g.oldSubscription:
+		op = subscriptionsCollection.update_one(query, {'$set': {
+			'valid': False,
+		}})
+		if op.modified_count > 0:
+			result = 'oldUpdated'
+
+	return success_json(data={
+		'result': result,
+		'alreadyExists': g.alreadyExists,
+		'subscriptionId': g.subscriptionId if not g.alreadyExists else None
+	})
